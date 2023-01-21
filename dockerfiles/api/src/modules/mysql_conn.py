7f1111cc19c3989063edda8a215b55437ca8321b
yonetaku <e205714@ie.u-ryukyu.ac.jp>
2023-01-21 11:46:10 +0900

    modify calendar date into japanese from english

--- dockerfiles/api/src/modules/mysql_conn.py
+++ dockerfiles/api/src/modules/mysql_conn.py
@@ -35,6 +35,10 @@ df = pd.DataFrame( data_list ,columns=['question','answer','source'])
 df.to_csv("./modules/text/csv/schedule.csv", index=None)   
 connection.commit()
 connection.close()
+l = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+w = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+nl = ["1","2","3","4","5","6","7","8","9","10","11","12"]
+nw = ["月","火","水","木","金","土","日"]
 
 def mysqlInit():
     connection = MySQLdb.connect(
@@ -74,6 +78,10 @@ def mysqlUpdate(content):
     dayId   = content["day"] 
     uid   = content["uid"] 
     dayTime   = content["dayTime"] 
+    dayTime = dayTime.split(',')
+    num2 = [ind for ind,elem in enumerate(l) if elem in dayTime[2]][0]
+    dayTime[2] = dayTime[2].replace(l[num2],nl[num2]).replace(" ","/")
+    dayTime = dayTime[0] + dayTime[2] + nw[[ind for ind,elem in enumerate(w) if elem == dayTime[1]][0]]
     cursor.execute("UPDATE schedule set title = %s where day = %s and dayId = %s and uid = %s and dayTime = %s",(title,dayId,titleId,uid,dayTime))
     data_list = []
     query = 'SELECT title,dayTime,source FROM schedule'
@@ -92,7 +100,11 @@ def mysqlPush(content):
     dayId   = content["day"] 
     uid   = content["uid"] 
     dayTime   = content["dayTime"] 
-    source = "user1" + "さん"
+    dayTime = dayTime.split(',')
+    num2 = [ind for ind,elem in enumerate(l) if elem in dayTime[2]][0]
+    dayTime[2] = dayTime[2].replace(l[num2],nl[num2]).replace(" ","/")
+    dayTime = dayTime[0] + dayTime[2] + nw[[ind for ind,elem in enumerate(w) if elem == dayTime[1]][0]]
+    source = uid + "さん"
     cursor.execute("INSERT INTO schedule VALUES (0,%s,%s,%s,%s,%s,%s)",(title,dayId,titleId,uid,dayTime,source))
     query = 'SELECT title,dayTime,source FROM schedule'
     cursor.execute(query)
@@ -111,7 +123,8 @@ def mysqlDel(content):
     dayId   = content["day"] 
     uid   = content["uid"] 
     dayTime   = content["dayTime"] 
-    cursor.execute("DELETE FROM schedule WHERE title = %s and day = %s and dayId = %s and uid = %s and dayTime = %s",(title,dayId,titleId,uid,dayTime))
+    dayTime = dayTime.split(',')
+    cursor.execute("DELETE FROM schedule WHERE title = %s and day = %s and dayId = %s and uid = %s",(title,dayId,titleId,uid))
     query = 'SELECT title,dayTime,source FROM schedule'
     cursor.execute(query)
     data_list = []
